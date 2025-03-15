d9e956f32ab0c902ab9162c187879b52d18fa959
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-03-15 13:44:49 -0700

    Start implementing AI for obtaining job info

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import requests, argparse
+import requests, argparse, openai, os
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 def read_job_urls(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -8,9 +9,12 @@ def read_job_urls(file_path):
 
 if __name__ == "__main__":
 
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    client = openai.OpenAI()
     parser = argparse.ArgumentParser()
     parser.add_argument('input_jobs', help='List of job URLs')
     args = parser.parse_args()
 
     jobs = read_job_urls(args.input_jobs)
-    print(jobs)
+    first_job = jobs[0]
