c843eca01ad79ba1f0dd661a0160ca64594bdd56
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-03-15 13:24:29 -0700

    Write code to extract job URLs from text file

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from linkedin-agent!")
+import requests, argparse
+from bs4 import BeautifulSoup
 
+def read_job_urls(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = [line.strip() for line in f if line.strip()]
+    return lines
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_jobs', help='List of job URLs')
+    args = parser.parse_args()
+
+    jobs = read_job_urls(args.input_jobs)
+    print(jobs)
